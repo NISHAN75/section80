a7d40dac672574c3488264358cdb54c51f828a64
Nishan Das~ <nishandas880@gmail.com>
2023-04-15 23:40:58 +0600

    all is ok

---
+++ main.js
@@ -0,0 +1,14 @@
+// jQuery(".project-list").isotope();
+// // click funtion
+// $(".project-tile li").on('click', function(){  
+//     $(".project-tile li").removeClass("active");
+//     $(this).addClass("active");
+//     let selector = $(this).attr('data-filter');
+//     $(".project-list").isotope({
+//         filter: selector
+        
+//     });
+// });
+console.log("hi")
+// what is  js
+// how to remove a class in a elements
