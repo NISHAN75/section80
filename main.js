e90efa0dfce58eae5cfc52c6377924af8e293c79
Nishan Das~ <nishandas880@gmail.com>
2023-04-17 13:24:41 +0600

    all is ok

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-jQuery(".project-list").isotope();
-// click funtion
-$(".project-tile li").on('click', function(){  
-    $(".project-tile li").removeClass("active");
-    $(this).addClass("active");
-    let selector = $(this).attr('data-filter');
-    $(".project-list").isotope({
-        filter: selector
-        
+( function( $ ) {
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
     });
-});
+} )( jQuery );
